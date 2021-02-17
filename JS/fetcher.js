8d333067a1116c26be86b1506f17d32c874bd6c2
Alan Weik <alaan.m@hotmail.com>
2021-02-17 17:55:27 +0100

    Added working fetch function for weather.

---
+++ JS/fetcher.js
@@ -0,0 +1,39 @@
+// Foursquare
+const clientId="EN5DJDGHU5COJS5BSROVHN0DYKHCGRGJ0FSWAJZC3TS0GT5T";
+const clientSecret="PCK5DQLXAJ1H4LZKRF5T3M2ERT2LK3IXW2FRYTXMYPDZ53ZY";
+
+async function fetchWeather(city) {
+  const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  return weatherData;
+}
+
+(async function addWeather(){
+    
+  var addweather = await fetchWeather("Malmö");
+})();
+
+
+
+    /*async function getCurrentWeather(city) {
+    const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
+
+    const Weather_api_url = new URL(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
+      );
+        /*Weather_api_url.searchParams.append("mode", "json");
+        Weather_api_url.searchParams.append("appid", appId);
+        Weather_api_url.searchParams.append("units", "metric");
+        Weather_api_url.searchParams.append("lang", "en");
+        const response = await fetch(Weather_api_url);
+        const WeatherData = await response.json();
+        console.log(WeatherData)
+        return WeatherData;
+    }*/
+
+    
+
+
+

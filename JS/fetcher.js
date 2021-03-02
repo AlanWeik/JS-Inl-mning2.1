6996515f6227a09cd034610e9219bec00a45ce39
Alan Weik <alaan.m@hotmail.com>
2021-03-02 16:03:10 +0100

    Trying a new approach. Put functions insinde eventhandler.

--- JS/fetcher.js
+++ JS/fetcher.js
@@ -1,23 +1,27 @@
 // Foursquare
-const clientId="EN5DJDGHU5COJS5BSROVHN0DYKHCGRGJ0FSWAJZC3TS0GT5T";
-const clientSecret="PCK5DQLXAJ1H4LZKRF5T3M2ERT2LK3IXW2FRYTXMYPDZ53ZY";
+const clientId = "EN5DJDGHU5COJS5BSROVHN0DYKHCGRGJ0FSWAJZC3TS0GT5T";
+const clientSecret = "PCK5DQLXAJ1H4LZKRF5T3M2ERT2LK3IXW2FRYTXMYPDZ53ZY";
+// Openweather
+const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
+const urlToFetch = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
+const city = document.querySelector("#city");
 
-async function fetchWeather(city) {
-  const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`);
-  const weatherData = await response.json();
-  console.log(weatherData);
-  return weatherData;
-}
+const executeSearch = () => {
+  document.querySelector("#button").onclick = async function fetchWeather(
+    city
+  ) {
+    const response = await fetch(urlToFetch);
+    const weatherData = await response.json();
+    console.log(weatherData);
+    return weatherData;
+  };
+};
 
-(async function addWeather(){
-    
+/*(async function () {
   var addweather = await fetchWeather("Malmö");
-})();
+})();*/
 
-
-
-    /*async function getCurrentWeather(city) {
+/*async function getCurrentWeather(city) {
     const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
 
     const Weather_api_url = new URL(
@@ -32,8 +36,3 @@ async function fetchWeather(city) {
         console.log(WeatherData)
         return WeatherData;
     }*/
-
-    
-
-
-

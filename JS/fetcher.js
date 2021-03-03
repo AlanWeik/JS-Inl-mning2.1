1ab3e9ce30072a4aafae1ba113242a45c51cd95c
Alan Weik <alaan.m@hotmail.com>
2021-03-03 10:53:46 +0100

    Working fetch functions for venues and weather now added.

--- JS/fetcher.js
+++ JS/fetcher.js
@@ -1,38 +1,36 @@
 // Foursquare
 const clientId = "EN5DJDGHU5COJS5BSROVHN0DYKHCGRGJ0FSWAJZC3TS0GT5T";
 const clientSecret = "PCK5DQLXAJ1H4LZKRF5T3M2ERT2LK3IXW2FRYTXMYPDZ53ZY";
-// Openweather
-const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
-const urlToFetch = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
-const city = document.querySelector("#city");
 
-const executeSearch = () => {
-  document.querySelector("#button").onclick = async function fetchWeather(
-    city
-  ) {
-    const response = await fetch(urlToFetch);
-    const weatherData = await response.json();
-    console.log(weatherData);
-    return weatherData;
-  };
-};
+async function fetchWeather(city) {
+  const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
+  const weatherUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`;
+  const response = await fetch(weatherUrl);
+  const weatherData = await response.json();
+  console.log(weatherData);
+  return weatherData;
+}
 
-/*(async function () {
+(async function () {
   var addweather = await fetchWeather("Malmö");
-})();*/
+})();
 
-/*async function getCurrentWeather(city) {
-    const weatherKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
+async function getVenues(city) {
+  const url = `https://api.foursquare.com/v2/venues/explore?near=`;
+  const venueUrl =
+    url +
+    city +
+    "&limit=10&client_id=" +
+    clientId +
+    "&client_secret=" +
+    clientSecret +
+    "&v=20210301";
+  const response = await fetch(venueUrl);
+  const venueData = await response.json();
+  console.log(venueData);
+  return venueData;
+}
 
-    const Weather_api_url = new URL(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherKey}`
-      );
-        /*Weather_api_url.searchParams.append("mode", "json");
-        Weather_api_url.searchParams.append("appid", appId);
-        Weather_api_url.searchParams.append("units", "metric");
-        Weather_api_url.searchParams.append("lang", "en");
-        const response = await fetch(Weather_api_url);
-        const WeatherData = await response.json();
-        console.log(WeatherData)
-        return WeatherData;
-    }*/
+(async function () {
+  var addVenue = await getVenues("Malmö");
+})();
